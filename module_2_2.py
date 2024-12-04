94799c2412f4c406df30fecfe46e0b0c828f1a81
BusAlena <alenkabusygina8507@gmail.com>
2024-12-04 16:22:55 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,25 @@
+# Вариант 1
+first = 90
+second = 102
+third = 90
+if first==second==third:
+    print ('3')
+elif first == second :
+    print('2')
+elif second == third:
+    print('2')
+elif first == third:
+   print('2')
+elif first!=second!=third:
+    print("0")
+
+# Вариант 2
+ # first = 90
+# second = 90
+# third = 90
+# if first==second==third:
+#     print ('3')
+# elif first == second or second == third or first == third:
+#     print('2')
+# elif first!=second!=third:
+#     print("0")
